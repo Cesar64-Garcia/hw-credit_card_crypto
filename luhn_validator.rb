3f38bd6f49205ad937bf855425c13580bc59e85b
César Garcia (希 賽) <cesar.garcia.mazariegos@outlook.com>
2022-03-05 15:52:34 +0800

    luhn_validator implemented

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Luhn algorithm validator
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -7,5 +10,11 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    raise 'No valid credit card number provided' if nums_a.count < 2
+
+    sum = nums_a[0...-1].each_with_index.reduce(0) do |t, (d, i)|
+      t + (d.to_i * (i.even? ? 1 : 2)).digits.sum
+    end
+    nums_a.last == (10 - sum % 10) % 10
   end
 end

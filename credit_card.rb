0af964b65c8a355be98ade35ca1d0bfb58220c1b
César Garcia (希 賽) <cesar.garcia.mazariegos@outlook.com>
2022-03-05 17:11:07 +0800

    hash implemented

--- credit_card.rb
+++ credit_card.rb
@@ -2,6 +2,7 @@
 
 require_relative './luhn_validator'
 require 'json'
+require 'digest'
 
 # Credit card
 class CreditCard
@@ -19,7 +20,7 @@ class CreditCard
 
   # returns json string
   def to_json(*_args)
-    hash.to_json
+    to_hash.to_json
   end
 
   # returns all card information as single string
@@ -28,22 +29,31 @@ class CreditCard
   end
 
   # return a new CreditCard object given a serialized (JSON) representation
-  def self.from_s(card_s)
-    card_ruby bench/luhn_bench.rb
+  def self.from_s(_card_s)
+    card_ruby bench / luhn_bench.rb
+  end
+
+  def to_hash
+    {
+      number: number,
+      expiration_date: expiration_date,
+      owner: owner,
+      credit_network: credit_network
+    }
   end
 
   # return a hash of the serialized credit card object
   def hash
-    # TODO: implement this method
     #   - Produce a hash (using default hash method) of the credit card's
     #     serialized contents.
     #   - Credit cards with identical information should produce the same hash
+    to_hash.hash
   end
 
   # return a cryptographically secure hash
   def hash_secure
-    # TODO: implement this method
     #   - Use sha256 from openssl to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    Digest::SHA256.hexdigest(to_json)
   end
 end
